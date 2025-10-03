013ed09a367bce19812267cec6e903e664f38266
monootech <52410699+monootech@users.noreply.github.com>
2025-10-04 01:37:33 +0300

    Update next.config.js

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,11 +1,10 @@
-// FILE: my_repo/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
+    NEXT_PUBLIC_R2_PUBLIC_URL: process.env.NEXT_PUBLIC_R2_PUBLIC_URL,
     PUBLISHER_WORKFLOW_URL: process.env.PUBLISHER_WORKFLOW_URL,
-    NEXT_PUBLIC_R2_PUBLIC_URL: process.env.NEXT_PUBLIC_R2_PUBLIC_URL
-  }
+  },
 }
 
 module.exports = nextConfig
