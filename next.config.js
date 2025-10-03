371daf12bd8dc14f152400eb50667eade046ea0e
monootech <52410699+monootech@users.noreply.github.com>
2025-10-03 19:44:31 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
-  // Remove the 'api' section as it's not valid in Next.js config
-}
-
-export default nextConfig;
