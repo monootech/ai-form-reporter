41ebda9034bafdb6e15cc2acac2dbed2812ca93d
monootech <52410699+monootech@users.noreply.github.com>
2025-10-08 03:30:47 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,1 @@
 // FILE: pages/_app.js
-import '../styles/globals.css'
-import { AnimatePresence } from 'framer-motion'
-
-/**
- * MyApp wraps every page. AnimatePresence enables page transition animations.
- */
-export default function MyApp({ Component, pageProps, router }) {
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      <Component {...pageProps} key={router.asPath} />
-    </AnimatePresence>
-  )
-}
